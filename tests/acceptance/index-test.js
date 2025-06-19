47c6293d492ec2116d1a226494812815cd50445a
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2025-06-19 10:59:04 -0600

    fix eslint rules

--- tests/acceptance/index-test.js
+++ tests/acceptance/index-test.js
@@ -1,5 +1,5 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, select, click } from '@ember/test-helpers';
+import { visit, click } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | Index', function(hooks) {
