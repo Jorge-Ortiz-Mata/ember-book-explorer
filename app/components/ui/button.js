5fa4a10ad0aa56775b1b4326d739173cc06a0376
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2025-06-18 18:25:11 -0600

    add button to fetch books using the open library API

---
+++ app/components/ui/button.js
@@ -0,0 +1,9 @@
+import Component from "@glimmer/component";
+import { action } from "@ember/object";
+
+export default class UiButtonComponent extends Component {
+  @action
+  handleOnClick() {
+    this.args.onClick();
+  }
+}
--- app/controllers/explorer.js
+++ app/controllers/explorer.js
@@ -1,12 +1,15 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
+import { service } from "@ember/service";
 import { tracked } from "@glimmer/tracking";
 
 export default class ExplorerController extends Controller {
   @tracked topicSelected = "N/A";
+  @tracked books = undefined;
+  @service open;
 
   topicOptions = [
-    { label: "Selecciona un tema", value: "" },
+    { label: "Selecciona un tema", value: "N/A" },
     { label: "Science Fiction", value: "science_fiction" },
     { label: "Fantasy", value: "fantasy" },
     { label: "Romance", value: "romance" },
@@ -18,4 +21,12 @@ export default class ExplorerController extends Controller {
   onChange(value) {
     this.topicSelected = value;
   }
-}
+
+  @action
+  async getBooks() {
+    if (this.topicSelected == "N/A") return;
+  
+    const data = await this.open.fetchBooks("horror");
+    this.books = data;
+  };
+};
